bba6108f94000f7f347a6e327baaafcbbdd9cd9b
fogouwilliam4@gmail.com <wr3n@DESKTOP-S566DAO.localdomain>
2026-06-25 18:05:20 +0100

    feat:Premier commit

---
+++ date.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+aujourdhui=$(date "+%d/%m/%y")
+heure_actuelle=$(date "+%H:%M:%S")
+
+echo -e  "Bonjour William, aujourd'hui nous somme le : $aujourdhui"
+echo "Il est : $heure_actuelle"
+
+#2eme facon de faire :
+# date +%H:%M:%S %d/%m/%y
+
